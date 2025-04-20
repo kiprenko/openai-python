137f69c21f6fb1106e9eaa4c5699c51a0505a554
kiprenko <zhenyakiprenko@gmail.com>
2025-04-20 17:13:12 +0200

    Add possibility to structure the note

--- chat_completions_api/audio_transcription/audio_notes_transcriptor.py
+++ chat_completions_api/audio_transcription/audio_notes_transcriptor.py
@@ -2,7 +2,9 @@ import sys
 from openai import OpenAI
 from dotenv import load_dotenv
 
+
 TRANSCRIPTION_MODEL = "gpt-4o-transcribe"
+STRUCTURE_MODEL = "gpt-4.1"
 
 
 def main():
@@ -10,17 +12,42 @@ def main():
     if args_count < 2:
         print("Usage: python audio_notes_transcriptor.py <path-to-audio-file>")
         sys.exit(1)
+    load_dotenv()
     path_to_audio_file = sys.argv[1]
     audio_file = open(path_to_audio_file, "rb")
-    load_dotenv()
     client = OpenAI()
 
-    transcription = client.audio.transcriptions.create(
+    transcription = transcript(client, audio_file)
+    structured_transcription = structure(client, transcription.text)
+
+    print(structured_transcription)
+
+
+def transcript(client, audio_file):
+    return client.audio.transcriptions.create(
         model=TRANSCRIPTION_MODEL,
         file=audio_file
     )
 
-    print(transcription.text)
+def structure(client, transcription):
+    return client.chat.completions.create(
+    model=STRUCTURE_MODEL,
+    messages=[
+        {
+            "role": "system",
+            "content": "Your task is to structure and organise transcriptions of audio notes. "
+                       "You accept a transcription and provide the structured version of it in the same language. "
+                       "Do not use Markdown, emoji or any other text formatting. "
+                       "Make the text laconic and easy to read, get rid of any vermin words. "
+                       "The result must look like "
+                       "a title and a paragraph with the summary of the given audio note transcription."
+        },
+        {
+            "role": "user",
+            "content": transcription
+        }
+    ]
+    ).choices[0].message.content
 
 
 if __name__ == "__main__":
